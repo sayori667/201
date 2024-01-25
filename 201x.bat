a65a3971b10509d33c9f2d6a8b684b38cf0d15ac
sayori667 <137815330+sayori667@users.noreply.github.com>
2024-01-25 20:55:06 +0100

    Add files via upload

---
+++ 201x.bat
@@ -0,0 +1,1 @@
+powershell -Command "Add-Type -AssemblyName System.Speech; $synth = New-Object -TypeName System.Speech.Synthesis.SpeechSynthesizer; $synth.Speak('tu viens de te faire baisé ton pc force a toi')"
